c4d1dc34876cc1d219952de35b126ff023a17d21
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-05-17 13:48:10 +0700

    update

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ window.addEventListener('load', () => {
 
 // Quote 
 const quotes = [
+	{ text: "I'll go beyond until the unknown end.", author: "Miyano Mamoru「ZERO to INFINITY」" },
 	{ text: "Keep walking until it is granted. The light form that day is still pushing you from behind. Running past without noticing even the setting sun.", author: "Chotokkyu「Starlight」" },
 	{ text: "Someday, just a little more. When the gеntle wind blows through the world, will anything change?", author: "Mrs. GREEN APPLE「lulu.」" },
 	{ text: "Sound of the teardrop is the sound of raindrop on the flower petal, give sorrow a blue sky. If shadow looms ahead on this ongoing road, think of an idea to get by.", author: "Gen Hoshino「Idea」" },
