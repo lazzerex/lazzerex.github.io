9732714f147c7f6d67189455f2c572b7c51cffc6
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-05-20 22:11:41 +0700

    update

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ window.addEventListener('load', () => {
 
 // Quote 
 const quotes = [
+	{ text: "Run together towards the future. Be just the way you are.", author: "Access「Arc jump’n to the sky」" },
 	{ text: "Maybe it's a gift that I couldn't recognize. Maybe I don't really need to feel satisfied. Maybe it's a gift that I spend all this time. Just trying to feel alive.", author: "Porter Robinson「Trying to Feel Alive」" },
 	{ text: "I'll go beyond until the unknown end.", author: "Miyano Mamoru「ZERO to INFINITY」" },
 	{ text: "Keep walking until it is granted. The light form that day is still pushing you from behind. Running past without noticing even the setting sun.", author: "Chotokkyu「Starlight」" },
